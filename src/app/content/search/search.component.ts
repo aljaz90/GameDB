29082d280f95835d25b9ed3049f9860b371f1a0c
Aljaz <aljaz1.kern@gmail.com>
2018-07-04 14:44:22 +0200

    Added fetch

--- src/app/content/search/search.component.ts
+++ src/app/content/search/search.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
+  querry;
   constructor() { }
 
   ngOnInit() {
+  }
+  search(){
+    console.log(this.querry)
+    fetch('https://api-endpoint.igdb.com/games/?search=Halo',{
+      headers: {
+        "Accept": "application/json",
+        "user-key": "2eba42ae20fcc61c3ab4e3917ea36856"
+      }
+    })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      console.log(myJson);
+    });
   }
 
 }
