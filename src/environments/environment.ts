cd3b8fdb0de9d566184e3c2b7fa09bb6d247f76a
Aljaz <aljaz1.kern@gmail.com>
2018-07-06 13:20:04 +0200

    Production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDCNSL9VLA24JVQSySAcVq0SbDN1CHsb4U",
-    authDomain: "gamedb-2018.firebaseapp.com",
-    databaseURL: "https://gamedb-2018.firebaseio.com",
-    projectId: "gamedb-2018",
-    storageBucket: "gamedb-2018.appspot.com",
-    messagingSenderId: "943168966336"
+
   }
 };
 
